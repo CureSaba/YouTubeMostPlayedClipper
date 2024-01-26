f591abc8a0a6610865f1843eb99f4d6acdabd920
CureSaba <94890302+CureSaba@users.noreply.github.com>
2024-01-26 12:51:00 +0900

    Update youtubemost.py

--- youtubemost.py
+++ youtubemost.py
@@ -1,58 +1,43 @@
 import re
-import http.client
-import logging
 import socket
 from urllib.request import Request, urlopen
-import sys
 import json
-import time
-import os
 
 dlurl = input("YouTubeURL: ")
+class (url):
+    def __init__:
+    def _execute_request(url, method=None, headers=None, data=None, timeout=socket._GLOBAL_DEFAULT_TIMEOUT):
+        base_headers = {"User-Agent": "Mozilla/5.0", "accept-language": "en-US,en"}
+        if headers:
+            base_headers.update(headers)
+        if data:
+            # encode data for request
+            if not isinstance(data, bytes):
+                data = bytes(json.dumps(data), encoding="utf-8")
+        if url.lower().startswith("http"):
+            request = Request(url, headers=base_headers, method=method, data=data)
+        else:
+            raise ValueError("Invalid URL")
+        return urlopen(request, timeout=timeout)  # nosec
+    
+    def get(url, extra_headers=None, timeout=socket._GLOBAL_DEFAULT_TIMEOUT):
+        if extra_headers is None:
+            extra_headers = {}
+        response = _execute_request(url, headers=extra_headers, timeout=timeout)
+        return response.read().decode("utf-8")
 
-
-def _execute_request(
-    url, method=None, headers=None, data=None, timeout=socket._GLOBAL_DEFAULT_TIMEOUT
-):
-    base_headers = {"User-Agent": "Mozilla/5.0", "accept-language": "en-US,en"}
-    if headers:
-        base_headers.update(headers)
-    if data:
-        # encode data for request
-        if not isinstance(data, bytes):
-            data = bytes(json.dumps(data), encoding="utf-8")
-    if url.lower().startswith("http"):
-        request = Request(url, headers=base_headers, method=method, data=data)
-    else:
-        raise ValueError("Invalid URL")
-    return urlopen(request, timeout=timeout)  # nosec
-
-
-def get(url, extra_headers=None, timeout=socket._GLOBAL_DEFAULT_TIMEOUT):
-    if extra_headers is None:
-        extra_headers = {}
-    response = _execute_request(url, headers=extra_headers, timeout=timeout)
-    return response.read().decode("utf-8")
-
-
-data = get(dlurl)
-a = (
-    "{"
-    + data[
-        data.find('"markerType":"MARKER_TYPE_HEATMAP"')
-        + 35 : data.rfind('"icon":"UNKNOWN"}')
-        + 19
-    ]
-    + "}"
-)
-a = json.loads(a)
-# print(a)
-try:
+    def list(self, ):
+    data = get(dlurl)
+    a = ("{" + data[data.find('"markerType":"MARKER_TYPE_HEATMAP"') + 35 : data.rfind('"icon":"UNKNOWN"}') + 19] + "}")
+    a = json.loads(a)
+    try:
     print(a["markersDecoration"])
     for i in a["markersDecoration"]["timedMarkerDecorations"]:
         print(i["decorationTimeMillis"])
-except:
-    print("Most played not found")
-    sys.exit()
-# 1s=1000ms
-mosttime = a["markersDecoration"]["timedMarkerDecorations"]
+    except:
+        print("Most played not found")
+    # 1s=1000ms
+    mosttime = a["markersDecoration"]["timedMarkerDecorations"]
+    for i in mosttime:
+    i = i["decorationTimeMillis"]
+    edittime = i / 1000  # sec
